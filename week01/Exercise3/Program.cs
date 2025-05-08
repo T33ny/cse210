b712c0131de16e154fc4d778d64ebe7e9388b4f0
T33ny <heather2bcute@hotmail.com>
2025-05-08 11:48:00 -0400

    1st_attempt

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1, 101);
+        Console.Write("Choose a number between 1-100: ");
+        string number_String = Console.ReadLine();
+        int guess = int.Parse(number_String);
+
+        while (guess != num)
+        {
+            if (guess > num)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("Guess again: ");
+                number_String = Console.ReadLine();
+                guess = int.Parse(number_String);
+            }
+            else if (guess < num)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("Guess again: ");
+                number_String = Console.ReadLine();
+                guess = int.Parse(number_String);
+            }
+        }  
+        Console.WriteLine("You guessed it!");
     }
 }
